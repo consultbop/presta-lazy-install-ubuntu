335765bb9bfd065d6899a90c27029c221c7dd3f4
consultbop <consultbop@gmail.com>
2022-09-12 18:18:12 +0530

    Initial Setup files for Prestashop

---
+++ README.md
@@ -0,0 +1,15 @@
+# Installation
+Use install.sh only for installation on a Ubuntu 20.04 machine.
+This script is mainly for reference. 
+
+# Architecture
+_LAMP_
+- Linux : Ubuntu 20.04
+- Apache : Apache2
+- MySQL : Version 5.7 (_Seems like DigitalOcean only supports 8. The whole configuration will have to be broken up and PHP extenstions will be needed_)
+- PHP : PHP 7.2
+
+# Prestashop Version
+Currently using 1.7.8.1 version for setting the Prestashop CMS as required.
+Make sure the theme and module version are compatible with 1.7.8.1.
+If you are looking to change the version of Prestashop CMS, make sure you make that change in your install.sh script file.
---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#install docker
+sudo apt update -y
+sudo apt-get upgrade -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update -y
+sudo apt install docker-ce -y
+
+#install Apache2
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt install apache2
+sudo ufw allow in "Apache"
+
+#install docker-compose
+sudo apt install docker-compose -y
+
+#Setup Apache config file
+mv ./prestawebsite.conf /etc/apache2/sites-available/
+sudo ln -s /etc/apache2/sites-available/prestawebsite.conf /etc/apache2/sites-enabled/prestawebsite.conf
+
+sudo systemctl restart apache2
+
+#install PHP
+sudo apt install php libapache2-mod-php php-mysql -y
+sudo apt-get install php-cli php-common php-mbstring php-gd php-intl php-xml php-mysql php-zip php-curl php-xmlrpc -y
+sudo systemctl restart apache2
+
+sudo apt-get install unzip -y
+
+#enable mod_rewrite
+sudo a2enmod rewrite
+sudo a2ensite prestawebsite
+
+
+#download prestashop
+cd /var/www/html
+rm index.html
+sudo wget https://download.prestashop.com/download/releases/prestashop_1.7.8.1.zip
+sudo unzip prestashop_1.7.8.1.zip
+
+cd 
+
+sudo chown www-data: /var/www/html/
+sudo chmod -R 755 /var/www/html/
+
+#initiate docker for mysql
+cd presta-lazy-install-ubuntu
+docker-compose -f docker-compose.yml up --build -d
+
+# # mysql docker config
+# docker exec -ti diyverse-cms_mysql_1 bash
+# chown -R mysql:mysql /var/lib/mysql
+# mysql -u root -p 
